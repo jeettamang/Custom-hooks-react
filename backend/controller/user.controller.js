1776270ddee23089a44da5f381809b4e88e460d5
Jeet Tamang <jeettamang011@gmail.com>
2025-08-09 16:49:56 +0545

    Authentication(user) completed

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 import UserModel from "../model/user.model.js";
 import { sendVerificationEmail } from "../services/sendMail.js";
+import bcrypt from "bcryptjs";
 
 const register = async (req, res) => {
   try {
@@ -8,10 +9,11 @@ const register = async (req, res) => {
       throw new Error("All fields are required");
     const existUser = await UserModel.findOne({ email });
     if (existUser) throw new Error("User already exist");
+    const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = await UserModel.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     sendVerificationEmail(email);
     res.json({
@@ -23,4 +25,67 @@ const register = async (req, res) => {
     res.status(500).json({ message: error.message || "Registration failed" });
   }
 };
-export { register };
+
+const getAllUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const allUsers = await UserModel.find(id);
+    res.status(200).json({
+      message: "All users list",
+      allUsers,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Users not available",
+      error: error.message,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password, phone } = req.body;
+    if (password) {
+      if (typeof password !== "string") {
+        throw new Error("Password must be a string");
+      }
+    }
+    const userData = { name, email, password };
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    userData.password = hashedPassword;
+    const updatedUser = await UserModel.findByIdAndUpdate(id, userData, {
+      new: true,
+    });
+    if (!updatedUser) {
+      throw new Error("User not found");
+    }
+    res.status(200).json({
+      message: "User data is updated",
+      updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update the user data",
+      error: error.message,
+    });
+  }
+};
+const delteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deltedUser = await UserModel.findByIdAndDelete(id);
+    if (!deltedUser) {
+      throw new Error("User not found");
+    }
+    res.status(200).json({
+      message: "User deleted successfully",
+      deletedUserDetail: deltedUser,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to delete the user", error: error.message });
+  }
+};
+export { register, getAllUser, updateUser, delteUser };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,16 @@
 import express from "express";
-import { register } from "../controller/user.controller.js";
+import {
+  delteUser,
+  getAllUser,
+  register,
+  updateUser,
+} from "../controller/user.controller.js";
 const router = express.Router();
 
-router.post("/register", register);
+router
+  .post("/register", register)
+  .get("/list", getAllUser)
+  .put("/update/:id", updateUser)
+  .delete("/delete/:id", delteUser);
 
 export default router;
